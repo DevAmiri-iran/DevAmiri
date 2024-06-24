afd019c51b68a990ed96f5055c68f2d6b258d449
mohammadMA68 <mohammad.m.a.6831@gmail.com>
2024-06-24 11:41:57 +0330

    Initial commit

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -1,7 +1,10 @@
 <?php
 use App\System;
 
-require_once 'src/vendor/autoload.php';
+if (file_exists('src/vendor/autoload.php'))
+    require_once 'src/vendor/autoload.php';
+else
+    die('To run this project, Composer must be installed.');
 
 define('ROOT', dirname(__FILE__, 2));
 define('APP', dirname(__FILE__));
