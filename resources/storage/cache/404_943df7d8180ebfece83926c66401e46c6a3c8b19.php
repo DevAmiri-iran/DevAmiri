916504469ca531bfcb0ca12994e696611e18ffed
mohammadMA68 <mohammad.m.a.6831@gmail.com>
2024-06-24 11:03:32 +0330

    Initial commit

--- resources/storage/cache/404_943df7d8180ebfece83926c66401e46c6a3c8b19.php
+++
@@ -1,7 +0,0 @@
-<?php $_shouldextend[1]=1; ?>
-
-<?php $this->startSection('title', __('Not Found')); ?>
-<?php $this->startSection('code', '404'); ?>
-<?php $this->startSection('message', __('Not Found')); ?>
-
-<?php if (isset($_shouldextend[1])) { echo $this->runChild('errors.minimal'); } ?>
